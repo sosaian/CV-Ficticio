958a5f9bd40d5f76d65c8dc9673467a8f9146849
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-01-29 19:37:24 -0300

    Update to 2023-01-29-19 Version
    - Finished mobile version of website.
    - Changed submit button for a fake one that shows a snackbar onclick to notify the user that it's a fake form.
    - Readjusted some things in HTML, CSS and JS files

--- script.js
+++ script.js
@@ -91,3 +91,14 @@ function toggleTheme()
 		theme.setAttribute('href', 'light-theme.css');
 	}
 }
+
+function snackbar() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
